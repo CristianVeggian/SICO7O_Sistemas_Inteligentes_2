0c28f52efb2922195416eb38492e37bd06ad0417
Cristian Veggian Matias <48965999+CristianVeggian@users.noreply.github.com>
2023-08-30 14:20:04 -0300

    Complete implementation of the Adaline Algorithm
    This implementation is complete, though it requires further testing and optmization

--- Adaline.py
+++ Adaline.py
@@ -13,14 +13,12 @@ class Adaline:
         self.erro = erro
 
     def phi(self, v):
-        if v >= 0:
-            return 1
-        return 0
+        return v
 
     def treina(self, X: list | npt.ArrayLike, d: list | npt.ArrayLike):
 
-        self.W = np.array([random.random() for _ in range(len(X)-1)])
-
+        #self.W = np.array([random.random() for _ in range(len(X)-1)])
+        self.W = [0.5,0.5,0.5]
         X = ex.normalize(X)
         
         if type(d) is list:
@@ -29,10 +27,9 @@ class Adaline:
         epocas = 0
         erro = 1
         
-        while erro > self.erro and epocas < self.MAX_ITER:
-            epocas += 1
-            erro = False
+        while erro > self.erro and epocas < self.MAX_ITER: 
             counterD = -1
+            np.random.shuffle(X)
             for line in X:
                 counterD += 1
                 counterX = 0
@@ -42,13 +39,17 @@ class Adaline:
                     counterX += 1
                 v += self.bias * self.W[-1] 
                 v = self.phi(v)
-                if v != d[counterD]:
-                    self.W = self.W + self.n*(d[counterD]-v)*np.append(line, self.bias)
-                    erro = True
+                self.W = self.W + self.n*(d[counterD]-v)*np.append(line, self.bias)
+            epocas += 1
+            XparaErro = list()
+            for line in X:
+                XparaErro.append(self.W*np.append(line, self.bias))
+            XparaErro = np.array(XparaErro)
+            erro = (np.square(d - self.W*XparaErro)).mean()
 
     def predicao(self, X: list | npt.ArrayLike):
         if type(X) is list:
-            X = np.array(X) 
+            X = np.array(X)
             
         saida = list()
             
@@ -63,6 +64,6 @@ class Adaline:
             
         return saida
 
-pct = Adaline()
-pct.treina([[0,0],[0,1],[1,0],[1,1]], [[0],[1],[1],[1]])
-print(pct.predicao([[-1,1],[1,-1],[0,-1],[3,2]]))
+ada = Adaline()
+ada.treina([[0,0],[0,1],[1,0],[1,1]],[[0],[0],[0],[1]])
+print(ada.predicao([[1.1,1.1],[-0.1,-0.1]]))
