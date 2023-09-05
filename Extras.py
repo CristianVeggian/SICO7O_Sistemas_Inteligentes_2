d4d705948b3abf1a5e214bb05d2879ee987395c5
Cristian Veggian Matias <48965999+CristianVeggian@users.noreply.github.com>
2023-09-05 15:28:44 -0300

    Implemented confusion matrix
    Implementation of a confusion matrix function, using 1 and -1 as classifiers

--- Extras.py
+++ Extras.py
@@ -9,3 +9,21 @@ def normalize(X: list | npt.ArrayLike):
     desvioPadrao = X.std()
 
     return np.array([(x-media)/desvioPadrao for x in X])
+
+def confusionMatrix(y_real: list | npt.ArrayLike, y_predict: list | npt.ArrayLike):
+
+    output = {"tp":0, "fp":0, "tn":0, "fn":0}
+
+    print(y_real)
+    print(y_predict)
+
+    for yr, yp in zip(y_real, y_predict):
+        if yr == yp and yr == 1:
+            output["tp"] += 1
+        elif yr == yp and yr == -1:
+            output["tn"] += 1
+        elif yr != yp and yr == -1:
+            output["fp"] += 1
+        else:
+            output["fn"] += 1
+    return output
